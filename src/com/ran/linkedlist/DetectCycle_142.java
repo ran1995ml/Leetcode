61aef83b4bb3986273aacc89b22de43b476d02db
ran1995ml <ran1995ml@163.com>
2022-03-09 15:16:27 +0800

    total 66

--- src/com/ran/linkedlist/DetectCycle_142.java
+++ src/com/ran/linkedlist/DetectCycle_142.java
@@ -11,7 +11,19 @@ import com.ran.bean.ListNode;
  */
 public class DetectCycle_142 {
     public ListNode detectCycle(ListNode head) {
-        
-        return head;
+        if(head==null||head.next==null) return null;
+        ListNode p1 = head.next;
+        ListNode p2 = head.next.next;
+        while(p1!=p2){
+            if(p2==null||p2.next==null) return null;
+            p1 = p1.next;
+            p2 = p2.next.next;
+        }
+        p1 = head;
+        while (p1!=p2){
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return p1;
     }
 }
